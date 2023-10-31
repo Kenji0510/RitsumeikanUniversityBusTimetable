84e062c2abbe2bbc4eb3416ee2e261d6b5f3df94
kenji <yukkurireimupikacyuu@gmail.com>
2023-11-01 00:27:29 +0900

    ww

--- src/app/fetchDBData/fetchDBData.tsx
+++ src/app/fetchDBData/fetchDBData.tsx
@@ -2,8 +2,8 @@ import '../globals.css'
 
 
 export default async function FetchDBData({destination}: {destination: string}) {
-    const apiUrl: string = "http://192.168.0.25:5000/busTimeApi/";
-    //const apiUrl: string = "https://kapikapi0510.info/busTimeApi/";
+    //const apiUrl: string = "http://192.168.0.25:5000/busTimeApi/";
+    const apiUrl: string = "https://kapikapi0510.info/busTimeApi/";
     //const res = await fetch(apiUrl + destination, { cache: "no-cache" });
     const res = await fetch(apiUrl + destination);
     const data = await res.json();
